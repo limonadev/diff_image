adc717492c96db1e2d8d9f50a5dbaf76437bc6d5
limonadev <limonadev@whiteleaf.dev>
2020-10-10 16:52:47 -0500

    Added example of the new compare function

--- example/diff_image_example.dart
+++ example/diff_image_example.dart
@@ -1,4 +1,5 @@
 import 'package:diff_image/diff_image.dart';
+import 'package:http/http.dart' as http;
 
 final FIRST_IMAGE =
     'https://raw.githubusercontent.com/nicolashahn/diffimg/master/images/mario-circle-cs.png';
@@ -17,33 +18,45 @@ void main() async {
       ignoreAlpha: false,
       asPercentage: false,
     );
-    showLog('The difference between images is: $diff');
+    showLog('The difference between images is: ${diff.diffValue}');
 
     diff = await DiffImage.compareFromUrl(
       FIRST_IMAGE,
       SECOND_IMAGE,
       ignoreAlpha: false,
     );
-    showLog('The difference between images is: $diff percent');
+    showLog('The difference between images is: ${diff.diffValue} percent');
 
     diff = await DiffImage.compareFromUrl(
       FIRST_IMAGE,
       SECOND_IMAGE,
     );
-    showLog('The difference between images is: $diff percent');
+    showLog('The difference between images is: ${diff.diffValue} percent');
 
-    // Uncomment and run this ONLY if you are not on Dart Web (saveDiff is not
-    // supported on Dart Web yet). [saveDiff] allows you to save a png
-    // showing the differences between the two images.
+    // To allow more flexibility with the package, now it's possible to get
+    // the difference between two images directly from the local memory.
+    var firstImageFromMemory = await _loadImage(FIRST_IMAGE);
+    var secondImageFromMemory = await _loadImage(SECOND_IMAGE);
+    diff = DiffImage.compareFromMemory(
+      firstImageFromMemory,
+      secondImageFromMemory,
+    );
+    showLog('The difference between images is: ${diff.diffValue} percent');
+
+    // Uncomment and run this ONLY if you are not on Dart Web (saveDiffImg
+    // is not supported on Dart Web yet). [saveDiffImg] allows you to save
+    // a png showing the differences between the two images.
 
     /*
     diff = await DiffImage.compareFromUrl(
       FIRST_IMAGE,
       SECOND_IMAGE,
       ignoreAlpha: false,
-      saveDiff: true,
     );
-    showLog('The difference between images is: $diff percent');
+    showLog('The difference between images is: ${diff.diffValue} percent');
+    await DiffImage.saveDiffImage(
+      diffImg: diff.diffImg,
+    );
     */
   } catch (e) {
     showLog(e);
@@ -53,4 +66,10 @@ void main() async {
 void showLog(dynamic log) {
   // ignore: avoid_print
   print(log);
+}
+
+Future<Image> _loadImage(String imgSrc) async {
+  var response = await http.get(imgSrc);
+  var img = decodeImage(response.bodyBytes);
+  return img;
 }

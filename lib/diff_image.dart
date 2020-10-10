7de0d73abc35c42910a7b8543ded00dae4e4efcb
limonadev <limonadev@whiteleaf.dev>
2020-10-10 16:51:55 -0500

    Updated exports of the package

--- lib/diff_image.dart
+++ lib/diff_image.dart
@@ -2,6 +2,9 @@
 
 library diff_image;
 
+export 'package:image/image.dart';
 export 'src/diff_image_io.dart'
     if (dart.library.io) 'src/diff_image_io.dart'
     if (dart.library.html) 'src/diff_image_html.dart';
+
+export 'src/models/diff_img_result.dart';
